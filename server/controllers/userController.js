dfbf75365eedb896680dd31feacc8a4088190670
nishita-esrat <nishitaesrat120@gmail.com>
2025-02-06 00:13:02 +0600

    login and log-out is done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require("../modal/userModal");
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 
 // registration
 const registration = async (req, res) => {
@@ -28,4 +28,55 @@ const registration = async (req, res) => {
     return res.status(500).json({ message: "Server error" });
   }
 };
-module.exports = { registration };
+
+// login
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required",
+    });
+  }
+  const user = await User.findOne({ email }).select(
+    "password name email avatar createdAt updatedAt"
+  );
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "please sign up first",
+    });
+  }
+  try {
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return res.status(401).json({
+        success: false,
+        message: "invalid credentials",
+      });
+    }
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_KEY);
+    return res
+      .cookie("token", token, {
+        expires: new Date(Date.now() + 2 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      })
+      .status(200)
+      .json({
+        success: true,
+        user: user,
+      });
+  } catch (error) {
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+// log out
+const logout = (req, res) => {
+  return res
+    .clearCookie("token")
+    .status(200)
+    .json({ success: true, message: "Logged out" });
+};
+
+module.exports = { registration, login ,logout};
--- server/routers/userRoute.js
+++ server/routers/userRoute.js
@@ -1,8 +1,10 @@
 
 const express = require("express")
-const { registration } = require("../controllers/userController")
+const { registration, login, logout } = require("../controllers/userController")
 const router = express.Router()
 
 router.post("/sign-up",registration)
+router.get("/sign-in",login)
+router.get("/log-out",logout)
 
 module.exports = router
