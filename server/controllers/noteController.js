73200a30b79b4bb709f8499aea5b623cc869ca7e
nishita-esrat <nishitaesrat120@gmail.com>
2025-02-26 19:54:34 +0600

    all friends note route

--- server/controllers/noteController.js
+++ server/controllers/noteController.js
@@ -1,4 +1,5 @@
 const Note = require("../modal/noteModal");
+const Friend = require("../modal/friendModal");
 
 // new note
 const createNote = async (req, res) => {
@@ -51,7 +52,6 @@ const deleteNote = async (req, res) => {
       success: true,
       message: "note deleted successfully",
     });
-    
   } catch (error) {
     res.status(500).json({
       success: false,
@@ -60,4 +60,45 @@ const deleteNote = async (req, res) => {
   }
 };
 
-module.exports = { createNote, deleteNote };
+// get all friends note
+const allFriendsNote = async (req, res) => {
+  try {
+    const id = req.id;
+
+
+    const friends = await Friend.find({
+      $or: [{ sender_id: id }, { receiver_id: id }],
+      status: "accepted",
+    });
+
+
+    const friendIds = friends.map((friend) =>
+      friend.sender_id.toString() === id ? friend.receiver_id : friend.sender_id
+    );
+
+
+    if (friendIds.length === 0) {
+      return res.status(200).json({ success: true, notes: [] });
+    }
+
+
+    const notes = await Note.find({
+      user_id: { $in: friendIds },
+      not_allowed_users: { $nin: [id] }, // Exclude notes where user is restricted
+    }).sort({ createdAt: -1 });
+
+
+    return res.status(200).json({
+      success: true,
+      notes,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message || "Server error",
+    });
+  }
+};
+
+module.exports = { createNote, deleteNote , allFriendsNote };
--- server/routers/noteRoute.js
+++ server/routers/noteRoute.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const isAuthenticate = require("../middleware");
-const { createNote, deleteNote } = require("../controllers/noteController");
+const {
+  createNote,
+  deleteNote,
+  allFriendsNote,
+} = require("../controllers/noteController");
 const router = express.Router();
 
 router.post("/new-note", isAuthenticate, createNote);
 router.delete("/delete-note/:noteId", isAuthenticate, deleteNote);
+router.get("/all-friends-note", isAuthenticate, allFriendsNote);
 
 module.exports = router;
