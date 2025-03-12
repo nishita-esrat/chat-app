ecd869709bfff53c6829256a95948f72af965d8f
nishita-esrat <nishitaesrat120@gmail.com>
2025-03-12 16:16:05 +0600

    remove member route

--- server/controllers/groupController.js
+++ server/controllers/groupController.js
@@ -92,14 +92,12 @@ const addNewMember = async (req, res) => {
       `You have been added to the group ${group.group_name}`
     );
 
-
     return res.status(200).json({
       success: true,
       message: `User added successfully to ${group.group_name}`,
       data: group,
       notification,
     });
-
   } catch (error) {
     return res.status(500).json({
       success: false,
@@ -108,4 +106,72 @@ const addNewMember = async (req, res) => {
   }
 };
 
-module.exports = { createGroup, addNewMember };
+// remove member
+const removeMember = async (req, res) => {
+  try {
+    const { member, conversation_id } = req.body;
+    const user_id = req.id;
+
+    const conversation = await Conversation.findById(conversation_id);
+
+    if (!conversation) {
+      return res.status(404).json({
+        success: false,
+        message: "Conversation not found",
+      });
+    }
+
+    // Check if user is an admin
+    if (!conversation.admin_ids.includes(user_id)) {
+      return res.status(403).json({
+        success: false,
+        message: "You are not authorized to remove members from this group",
+      });
+    }
+
+    // Prevent admins from removing another admin
+    if (conversation.admin_ids.includes(member)) {
+      return res.status(403).json({
+        success: false,
+        message: "You cannot remove another admin from the group",
+      });
+    }
+
+    // Check if the new member is already in the group
+    if (!conversation.participants.includes(member)) {
+      return res.status(400).json({
+        success: false,
+        message: "User is not a participant in the group",
+      });
+    }
+
+    // remove new member
+    const group = await Conversation.findByIdAndUpdate(
+      conversation_id,
+      { $pull: { participants: member } },
+      { new: true } // Return updated conversation
+    );
+
+    const notification = await createNotification(
+      member,
+      user_id,
+      "group_activity",
+      `You have been removed to the group ${group.group_name}`
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: `User removed successfully to ${group.group_name}`,
+      data: group,
+      notification,
+    });
+    
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message || "Server error",
+    });
+  }
+};
+
+module.exports = { createGroup, addNewMember, removeMember };
--- server/routers/groupRoute.js
+++ server/routers/groupRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const isAuthenticate = require("../middleware");
-const { createGroup, addNewMember } = require("../controllers/groupController");
+const { createGroup, addNewMember, removeMember } = require("../controllers/groupController");
 const router = express.Router();
 
 router.post("/new-group", isAuthenticate, createGroup);
 router.put("/add-new-member", isAuthenticate, addNewMember);
+router.put("/remove-member", isAuthenticate, removeMember);
 
 module.exports = router;
