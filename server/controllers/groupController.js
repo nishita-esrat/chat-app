33707d7765f6a4a400abdb1e966ad6fe32da98f6
nishita-esrat <nishitaesrat120@gmail.com>
2025-03-12 16:03:13 +0600

    add new member route

--- server/controllers/groupController.js
+++ server/controllers/groupController.js
@@ -1,10 +1,12 @@
 const Conversation = require("../modal/conversationModal");
 const { createNotification } = require("../utility/notifications");
 
+// new group
 const createGroup = async (req, res) => {
   try {
     const { group_name, participants } = req.body;
     const admin = req.id;
+    const notification_participants = [...participants];
 
     if (!group_name || !participants || participants.length === 0) {
       return res.status(400).json({
@@ -25,7 +27,7 @@ const createGroup = async (req, res) => {
     });
 
     const notification = await createNotification(
-      participants,
+      notification_participants,
       admin,
       "group_activity",
       `You have been added to the group ${group.group_name}`
@@ -45,4 +47,65 @@ const createGroup = async (req, res) => {
   }
 };
 
-module.exports = { createGroup };
+// add member
+const addNewMember = async (req, res) => {
+  try {
+    const { new_member, conversation_id } = req.body;
+    const user_id = req.id;
+
+    const conversation = await Conversation.findById(conversation_id);
+
+    if (!conversation) {
+      return res.status(404).json({
+        success: false,
+        message: "Conversation not found",
+      });
+    }
+
+    // Check if user is an admin
+    if (!conversation.admin_ids.includes(user_id)) {
+      return res.status(403).json({
+        success: false,
+        message: "You are not authorized to add members to this group",
+      });
+    }
+
+    // Check if the new member is already in the group
+    if (conversation.participants.includes(new_member)) {
+      return res.status(400).json({
+        success: false,
+        message: "User is already a participant in the group",
+      });
+    }
+
+    // Add new member
+    const group = await Conversation.findByIdAndUpdate(
+      conversation_id,
+      { $push: { participants: new_member } },
+      { new: true } // Return updated conversation
+    );
+
+    const notification = await createNotification(
+      new_member,
+      user_id,
+      "group_activity",
+      `You have been added to the group ${group.group_name}`
+    );
+
+
+    return res.status(200).json({
+      success: true,
+      message: `User added successfully to ${group.group_name}`,
+      data: group,
+      notification,
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message || "Server error",
+    });
+  }
+};
+
+module.exports = { createGroup, addNewMember };
--- server/routers/groupRoute.js
+++ server/routers/groupRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const isAuthenticate = require("../middleware");
-const { createGroup } = require("../controllers/groupController");
+const { createGroup, addNewMember } = require("../controllers/groupController");
 const router = express.Router();
 
 router.post("/new-group", isAuthenticate, createGroup);
+router.put("/add-new-member", isAuthenticate, addNewMember);
 
 module.exports = router;
